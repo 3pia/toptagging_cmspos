bbe9af8edad67dffc984db18fd20f8c82b725095
JGlombitza <jonas.glombitza@rwth-aachen.de>
2019-09-11 14:28:16 +0200

    add cnn example add utils.py

---
+++ jet_cnn.py
@@ -0,0 +1,66 @@
+import numpy as np
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import utils
+layers = keras.layers
+
+# ---------------------------------------------------------
+# Boilerplate. You can ignore this part.
+# ---------------------------------------------------------
+folder = utils.get_submit_folder()  # do not remove this line!
+
+# ---------------------------------------------------------
+# Load and plot data
+# ---------------------------------------------------------
+images, labels = utils.load_data(type='images')  # load data
+print("shape of images", images.shape)
+print("shape of labels", labels.shape)
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+for i, ax in enumerate(axes):
+    idx = np.random.choice(np.where(labels == i)[0])
+    norm = LogNorm(10**-4, images.max(), clip='True')
+    im = ax.imshow(images[idx][..., -1], norm=norm)
+    ax.set_xlabel('eta')
+    ax.set_ylabel('phi')
+axes[0].set_title("qcd")
+axes[1].set_title("top")
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+fig.colorbar(im, cax=cbar_ax)
+fig.savefig(folder+'/random_images.png')
+
+# ---------------------------------------------------------
+# Build your model
+# ---------------------------------------------------------
+inp = layers.Input(shape=(40, 40, 1))
+y = layers.Conv2D(8, 3, activation='relu', padding='same')(inp)
+y = layers.MaxPooling2D(pool_size=(2, 2))(y)
+y = layers.Conv2D(16, 5, activation='relu', padding='same')(y)
+y = layers.MaxPooling2D(pool_size=(2, 2))(y)
+y = layers.Conv2D(32, 5, activation='relu', padding='same')(y)
+y = layers.Flatten()(y)
+y = layers.Dropout(0.1)(y)
+y = layers.Dense(2, activation="softmax")(y)
+
+model = keras.models.Model(inputs=inp, outputs=y)
+print(model.summary())  # print model details
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.Adam(0.001), metrics=["accuracy"])
+
+model.fit(images, labels, epochs=10, validation_split=0.1, batch_size=128, verbose=2,
+          callbacks=[keras.callbacks.CSVLogger(folder + '/history.csv')])
+
+# ---------------------------------------------------------
+# Plot your losses and results
+# ---------------------------------------------------------
+history = np.genfromtxt(folder+'/history.csv', delimiter=',', names=True)
+
+fig, ax = plt.subplots(1)
+ax.plot(history['epoch'], history['loss'],     label='training')
+ax.plot(history['epoch'], history['val_loss'], label='validation')
+ax.legend()
+ax.set(xlabel='epoch', ylabel='loss')
+fig.savefig(folder+'/loss.png')
---
+++ utils.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import tensorflow as tf
+import numpy as np
+
+
+def get_submit_folder():
+    """ Check submit details, remove warnings using tf logging warning and make directory
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    str
+        path of the created directory
+
+    """
+    try:
+        CONDOR_ID = os.environ['CONDOR_ID']
+    except KeyError:
+        sys.exit('Error: Run this script with "pygpu %file"')
+    tf.logging.set_verbosity(tf.logging.ERROR)
+
+    folder = 'train-CNN-%s' % CONDOR_ID  # folder for training results
+    os.makedirs(folder)
+
+    return folder
+
+
+def load_data(type):
+    """ Load toptagging data
+
+    Parameters
+    ----------
+    type : str
+        Which dataset should be loaded: 'images' or '4vectors'?
+
+    Returns
+    -------
+    np.array, np.array
+        tuple of images, labels
+
+    """
+    path = "/net/scratch/deeplearning/toptagging/"
+    if type == "images":
+        file = np.load(path + "images.npz")
+    elif type == "4vectors":
+        file = np.load(path + "vectors.npz")
+    else:
+        raise AssertionError("type should be 'images or '4vectors'")
+
+    images = file["images"][..., np.newaxis]
+    labels = file["labels"]
+
+    return (images, labels)
